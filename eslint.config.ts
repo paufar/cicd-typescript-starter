db62eb75f4952a88980a8ca620c85172a376a540
Paula <paulafarrugia95@gmail.com>
2025-09-14 17:40:58 +0200

    Fix security

--- eslint.config.ts
+++ eslint.config.ts
@@ -5,16 +5,7 @@ import { defineConfig } from "eslint/config";
 import pluginSecurity from "eslint-plugin-security";;
 
 export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-  },
-  {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    languageOptions: { globals: globals.node },
-  },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
   pluginSecurity.configs.recommended,
-  
 ]);
